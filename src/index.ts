e864b85dcdbb24823f00630f8c8556044ab00ddd
Komail Mohammadi <komail.mohammadi@evia.de>
2026-04-02 18:09:29 +0200

    Export iconMap from index.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,5 @@ export * from './icons/index';
 export { Icon } from './Icon';
 export { createWashIcon } from './createWashIcon';
 export { getIcon } from './getIcon';
+export { iconMap } from './iconMap';
 export type { WashIconProps, IconNode } from './types';
